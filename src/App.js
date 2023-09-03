ed2410f2dcbddf68e1d3d76ab608ad2c51dba773
samuelk14 <smho-rey14@hotmail.com>
2023-09-03 15:20:02 -0500

    Creacion del proyecto en React, se despliega la pagina del login, la cual realiza una consulta a la api de la actividad anterior, se verifican los datos y redirige a la pagina de acuerdo a las credenciales indicadas

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from './login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/login.jsx
@@ -0,0 +1,28 @@
+import React, { useRef} from 'react';
+import './login.css';//se importan los estilos
+
+
+
+function Login() {
+
+
+
+  return (
+    <div>
+      <section>      
+        <img src="https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png" alt="foto de perfil" />
+        <p>Por favor, ingrese su usuario y contraseña</p>
+        {/* se consume la API creada en el componente anterior */}
+        <form action="http://localhost/proyecto/formato-de-recepcion/conexion/login.php" method="POST"> 
+          {/* se crea el input para el suario */}       
+          <input type="text" placeholder="usuario" name="Nusuario" id="username" />
+          {/* input para a contraseña */}
+          <input type="password" placeholder="Ingresa contraseña" name="pass" id="password"/>
+          <input type="submit" value="INICIAR SESIÓN" />
+        </form>
+      </section>
+    </div>
+  );
+}
+
+export default Login;
